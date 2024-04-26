e284f0bcb3fd11704c3027d59eee3813710c0c25
szmanda <michalszmanda.mail@gmail.com>
2024-04-26 11:55:08 +0200

    MvcNews complete

---
+++ MvcNews/MvcNews/Models/NewsItem.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcNews.Models
+{
+    public class NewsItem
+    {
+        public int Id { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime Timestamp { get; set; }
+        [Required]
+        [DisplayName("News content")]
+        [StringLength(140, MinimumLength=5, ErrorMessage="Wrong length")]
+        public string Text { get; set; } = string.Empty;
+
+        [Timestamp]
+        public byte[] RowVersion { get; set; } = new byte[0];
+
+    }
+}
